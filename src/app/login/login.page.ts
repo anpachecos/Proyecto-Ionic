7b065a11a3731d95e80e24871814c9a4b501c197
anpachecos <an.pachecos@duocuc.cl>
2024-08-22 13:24:36 -0400

    LocalStorage null arreglado yupi

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -11,7 +12,7 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
 
-  constructor(public fb: FormBuilder) { 
+  constructor(public fb: FormBuilder, public alertController: AlertController) { 
 
     this.formularioLogin = this.fb.group({
       'nombre': new FormControl("", Validators.required),
@@ -24,4 +25,22 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario') || '{}'); /*Esta solución encontré en StackOverflow, 
+    pero hay que tener ojo si ha futuro tengo problemas con usuarios creados pero con datos Vacíos, 
+    ya que el operados OR pasará objetos localStorage con datos o vacíos.*/
+
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('Ingresado!!!');
+    } else {
+      const alert = await this.alertController.create({
+        header: '¡Usuario Incorrecto!',
+        message: 'Los datos ingresados no son válidos',
+        buttons: ['Aceptar'],
+      });
+
+    }
+  }
 }
