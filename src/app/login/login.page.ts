03b7ecf575020ed70a2b87dd976ea0ff43e4c774
anpachecos <an.pachecos@duocuc.cl>
2024-08-22 11:43:38 -0400

    Dejando el login bonito

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+
+  constructor(public fb: FormBuilder) { 
+
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required)
+
+    })
+
+  }
 
   ngOnInit() {
   }
