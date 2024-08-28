b36fe744e5eaef204cadad304d8d7e752c3364e3
anpachecos <an.pachecos@duocuc.cl>
2024-08-27 20:02:12 -0400

    Ingrese nombre y apellido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule], //puse esta wea para que me dejara usar componentes de formulario sin hacer un formulario, asumo que tendré que eliminarlo después para que no me genere problemas. 
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
